bf6970d4ae8e91dbbe792b22610700d48b4ba79b
Alexandra Egorova <egorova.aleksandra@gmail.com>
2026-02-20 18:49:17 +0200

    removed neon adapter

--- backend/src/prisma/prisma.service.ts
+++ backend/src/prisma/prisma.service.ts
@@ -1,10 +1,10 @@
 import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '../../generated/prisma/client.js';
-// import { PrismaPg } from '@prisma/adapter-pg'; // Импорт адаптера
-import { PrismaNeon } from '@prisma/adapter-neon';
+import { PrismaPg } from '@prisma/adapter-pg'; // Импорт адаптера
+// import { PrismaNeon } from '@prisma/adapter-neon';
 // import pg from 'pg';
-import { neonConfig } from '@neondatabase/serverless';
-import ws from 'ws';
+// import { neonConfig } from '@neondatabase/serverless';
+// import ws from 'ws';
 
 @Injectable()
 export class PrismaService
@@ -12,11 +12,11 @@ export class PrismaService
   implements OnModuleInit, OnModuleDestroy
 {
   constructor() {
-    neonConfig.webSocketConstructor = ws;
+    // neonConfig.webSocketConstructor = ws;
 
-    const adapter = new PrismaNeon({
-      // connectionString: process.env.DATABASE_URL!,
-      connectionString: process.env.DATABASE_URL,
+    const adapter = new PrismaPg({
+      connectionString: process.env.DATABASE_URL!,
+      // connectionString: process.env.DATABASE_URL,
     });
 
     super({ adapter });
